5996a55b22cd272a54e9172d9da4fd0e996394bc
Christopher Luna <dev@christopher-luna.com>
2015-06-18 10:03:01 -0500

    Removed Polymer

--- src/x-json.js
+++ src/x-json.js
@@ -1,9 +1,106 @@
-/*jslint indent: 2, newcap: true */
-/*global Polymer*/
+/*jslint indent: 2, nomen: true, regexp: true */
+/*global HTMLElement, map, window, document*/
 
 (function () {
   'use strict';
-  var collapseFunction;
+  var importDoc, collapseFunction, xjsonPrototype, properties, map;
+
+  map = Function.prototype.call.bind(Array.prototype.map);
+  importDoc = (document._currentScript || document.currentScript).ownerDocument;
+
+
+  /**
+   * Create and add the shadow root to obj.root
+   * If necessary, it will rewrite the style and add it to the document head
+   *
+   * @param {object} obj The element to add the shadow root.
+   * @param {string} idTemplate The id of the template element.
+   * @param {string} [elementName] The name element, used for rewriting the css.
+   *                 If omited, it will use the idTemplate as name.
+   */
+  function addShadowRoot(obj, idTemplate, elementName) {
+    var template, styleStr, newStyle, hasStyle, dummy;
+
+    obj.root = obj.createShadowRoot();
+    template = importDoc.querySelector('#' + idTemplate);
+    obj.root.appendChild(template.content.cloneNode(true));
+
+    hasStyle = /<style(.|\n)*<\/style>/m;
+    if (window.ShadowDOMPolyfill && hasStyle.test(template.innerHTML)) {
+      dummy = document.createElement('head');
+      dummy.innerHTML = template.innerHTML;
+      styleStr = map(dummy.getElementsByTagName('style'), function (style) {
+        return style.innerHTML
+          .replace(/:host/gm, elementName || idTemplate).replace(/::content/gm, '')
+          .trim();
+      }).join("\n");
+      dummy = null;
+
+      newStyle = document.createElement('style');
+      newStyle.innerHTML = styleStr;
+      document.getElementsByTagName('head')[0].appendChild(newStyle);
+    }
+  }
+
+
+
+  /**
+   * Uses Object.defineProperty to add setters and getters
+   * to each property of the element.
+   *
+   * Each property is reflected to the equivalent DOM attribute
+   * @param {object} obj The element to add the shadow root.
+   * @param {object} props Object with the properties.
+   */
+  function prepareProperties(obj, props) {
+    function toHyphens(str) {
+      return str.replace(/([A-Z])/g, '-$1').toLowerCase();
+    }
+    function convert(val, desc) {
+      if (desc.type === Number) {
+        return parseInt(val, 10);
+      }
+      if (desc.type === String) {
+        return String(val);
+      }
+      return val;
+    }
+    obj.props = {};
+
+    Object.keys(props).forEach(function (name) {
+      var attrName, desc, value;
+
+      desc = props[name];
+      attrName = toHyphens(name);
+      value = desc.value;
+      if (typeof value === 'function') {
+        value = value();
+      }
+
+      if (obj.getAttribute(attrName) === null) {
+        if (typeof obj.props[name] !== 'object') {
+          obj.setAttribute(attrName, obj.props[name]);
+        } else {
+          obj.setAttribute(attrName, value);
+        }
+      }
+      Object.defineProperty(obj, name, {
+        get : function () {
+          return obj.props[name] || convert(obj.getAttribute(attrName), desc) || value;
+        },
+        set : function (val) {
+          obj.props[name] = val;
+          if (typeof val !== 'object') {
+            obj.setAttribute(attrName, val);
+          }
+          if (typeof obj[desc.observer] === 'function') {
+            obj[desc.observer](val);
+          }
+        }
+      });
+    });
+  }
+
   function isCollapsible(obj) {
     return Array.isArray(obj) || (typeof obj === 'object' && obj !== null);
   }
@@ -84,38 +181,42 @@
   }
 
 
-  Polymer({
-    is : 'x-json',
-    properties : {
-      data : {
-        value : '',
-        observer : 'dataChanged'
-      }
-    },
-    dataChanged: function (data) {
-
-      if (data === '') {
-        this.$.container.innerHTML = '';
-        return;
-      }
-      if (typeof data !== 'string') {
-        data = JSON.stringify(data);
-      }
-
-      this.$.container.innerHTML = str(JSON.parse(data));
-
-
-      // Adding the function to collapse
-      (function (container) {
-        var list = Polymer.dom(container).querySelectorAll('.collapser');
-
-        Array.prototype.forEach.call(list, collapseFunction);
-      }(this.$.container));
-
-    },
-    ready : function () {
-      this.dataChanged(this.data);
+  xjsonPrototype = Object.create(HTMLElement.prototype);
+  properties = {
+    data : {
+      value : '',
+      observer : 'dataChanged'
     }
+  };
+  xjsonPrototype.dataChanged = function (data) {
+    var container = this.root.getElementById('container');
+
+    if (data === '') {
+      container.innerHTML = '';
+      return;
+    }
+    if (typeof data !== 'string') {
+      data = JSON.stringify(data);
+    }
+
+    container.innerHTML = str(JSON.parse(data));
+
+
+    // Adding the function to collapse
+    (function (container) {
+      var list = container.querySelectorAll('.collapser');
+
+      Array.prototype.forEach.call(list, collapseFunction);
+    }(container));
+
+  };
+  xjsonPrototype.createdCallback = function () {
+    addShadowRoot(this, 'x-json');
+    prepareProperties(this, properties);
+    this.dataChanged(this.data);
+  };
+  document.registerElement('x-json', {
+    prototype : xjsonPrototype
   });
 }());
 
